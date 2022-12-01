2093239d98d2765ceb96b3c9c5deeb1e843761f3
Erick140297 <erickmonterrubio67@outlook.com>
2022-12-01 15:55:26 -0600

    Varibles de entorno

--- src/db.js
+++ src/db.js
@@ -1,11 +1,12 @@
 const { connect } = require("mongoose");
+require('dotenv').config()
+
+const { URL } = process.env
 
 // If the MongoDB service doesn't not start. Open a terminal and run mongod
 
 const db = async () => {
-  // const conn = await connect("mongodb://localhost:27017/dbPF");
-  const conn = await connect(`mongodb+srv://riquiamado:c220471A@cluster0.56kysuf.mongodb.net/?retryWrites=true&w=majority`);
-  
+  const conn = await connect(URL);
   console.log("DB connected to", conn.connection.name);
 };
 
