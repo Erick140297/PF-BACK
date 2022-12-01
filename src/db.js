047ac7a18179547dc603efedac5fd93a10ee0c89
riquiamado <carlosamado221471@gmail.com>
2022-12-01 18:35:58 -0300

    conectando a mongodbatlas

--- src/db.js
+++ src/db.js
@@ -3,7 +3,9 @@ const { connect } = require("mongoose");
 // If the MongoDB service doesn't not start. Open a terminal and run mongod
 
 const db = async () => {
-  const conn = await connect("mongodb://localhost:27017/dbPF");
+  // const conn = await connect("mongodb://localhost:27017/dbPF");
+  const conn = await connect(`mongodb+srv://riquiamado:c220471A@cluster0.56kysuf.mongodb.net/?retryWrites=true&w=majority`);
+  
   console.log("DB connected to", conn.connection.name);
 };
 
