b00e612252b61203e17750573832b5fc617e1a3d
Erick140297 <erickmonterrubio67@outlook.com>
2022-12-02 08:22:21 -0600

    post_provider

--- src/routes/post_provider.js
+++ src/routes/post_provider.js
@@ -6,7 +6,7 @@ router.post("/provider", async (req, res) => {
   try {
     const { name, email, service } = req.body
     const provider = new Provider( { name, email, service })
-    const providerSave = await provider.save()
+    await provider.save()
     res.status(200).json({message: "Provider saved successfully"});
   } catch (error) {
     res.status(400).json("Error");
