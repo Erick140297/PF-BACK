1fda18fab36af569ba3a750125be3fdf7c05a597
faka02 <facundoezequielalonso@gmail.com>
2022-12-02 12:37:05 -0300

    probando

--- src/routes/get_providers.js
+++ src/routes/get_providers.js
@@ -5,6 +5,7 @@ const Provider = require('../models/provider')
 router.get("/providers", async (req, res) => {
   try {
     const providers = await Provider.find()
+    console.log("Hola,asd.")
     res.status(200).json(providers);
   } catch (error) {
     res.status(400).json("Error");
