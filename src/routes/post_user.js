00507cc3f2f499029832812f2fb926251d29ca8d
Paula Ramírez <104044317+PaulaAn1@users.noreply.github.com>
2022-12-06 10:51:11 -0500

    modeloUser

---
+++ src/routes/post_user.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+const router = Router();
+const User = require("../models/users");
+
+router.post("/users", async (req, res) => {
+  try {
+    const { name, email, password } = req.body
+    const user = new User({ name, email, password })
+    /* const user = new User(req.body); */
+    await user.save();
+    res.status(200).json({ message: "User saved successfully" });
+  } catch (error) {
+    res.status(400).json("Error");
+  }
+});
+
+module.exports = router;
