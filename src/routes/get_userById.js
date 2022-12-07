b55627eda81e52fdb49afed06a4cd193500437a6
Erick140297 <erickmonterrubio67@outlook.com>
2022-12-07 09:03:14 -0600

    carta routes user

--- src/routes/get_userById.js
+++ src/routes/get_userById.js
@@ -5,7 +5,7 @@ const User = require('../models/users')
 router.get("/users/:id", async (req, res) => {
   try {
     const { id } = req.params
-    const user = await User.find({id}).populate('services')
+    const user = await User.findById(id).populate('services')
     res.status(200).json(user)
   } catch (error) {
     res.status(400).json("Id not found");
