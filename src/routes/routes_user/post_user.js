ae4674133d2ed4882f8ca07cb039a3c8ebf5c46a
Erick140297 <erickmonterrubio67@outlook.com>
2022-12-08 10:54:09 -0600

    Encriptacion de la contraseña

--- src/routes/routes_user/post_user.js
+++ src/routes/routes_user/post_user.js
@@ -1,11 +1,16 @@
 const { Router } = require("express");
+const bcrypt = require('bcrypt')
 const router = Router();
 const User = require("../../models/users");
 
 router.post("/users", async (req, res) => {
   try {
     const { name, email, password } = req.body
-    const user = new User({ name, email, password })
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User({ name, email, passwordHash })
     await user.save();
     res.status(200).json({ message: "User saved successfully" });
   } catch (error) {
