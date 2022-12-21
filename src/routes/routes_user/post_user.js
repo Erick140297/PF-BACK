3c7c6f022e2c9da8eecbc308d95ba93389912872
faka02 <facundoezequielalonso@gmail.com>
2022-12-21 15:37:43 -0300

    auth0 a Google login

--- src/routes/routes_user/post_user.js
+++ src/routes/routes_user/post_user.js
@@ -7,14 +7,21 @@ router.post("/users", async (req, res) => {
   try {
     const { name, email, password } = req.body
     //const { name, email } = req.body
+    
+    if (password) {
+      const saltRounds = 10
+      const passwordHash = await bcrypt.hash(password, saltRounds)
+  
+      const user = new User({ name, email, passwordHash })
+      //const user = new User({ name, email })
+      await user.save();
+      res.status(200).json({ message: "User saved successfully" });      
+    } else {
+      const user = new User({ name, email })
+      await user.save();
+      res.status(200).json({ message: "User saved successfully" });
+    }
 
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(password, saltRounds)
-
-    const user = new User({ name, email, passwordHash })
-    //const user = new User({ name, email })
-    await user.save();
-    res.status(200).json({ message: "User saved successfully" });
   } catch (error) {
     console.log(error)
     res.status(400).json(error);
