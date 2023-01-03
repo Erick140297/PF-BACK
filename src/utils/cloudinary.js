3537390c3ddd5fc6b8945069800aacae4210c271
Erick140297 <erickmonterrubio67@outlook.com>
2023-01-02 19:06:14 -0600

    cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,15 +1,24 @@
 const cloudinary = require("cloudinary");
 require("dotenv").config();
 
-const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY } = process.env;
+// const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY } = process.env;
 
-const CLOUDINARY_API_SECRET = "477279579552534";
+// const CLOUDINARY_CLOUD_NAME = "dfaxzahb0"
+// const CLOUDINARY_API_SECRET = "477279579552534"
+// const CLOUDINARY_API_KEY = "8H7ZfZznu35WQ3bMH46QEXgWvLg"
 
-cloudinary.config({
-  cloud_name: CLOUDINARY_CLOUD_NAME,
-  api_key: CLOUDINARY_API_SECRET,
-  api_secret: CLOUDINARY_API_KEY,
-  secure: true,
+
+// cloudinary.config({
+//   cloud_name: CLOUDINARY_CLOUD_NAME,
+//   api_key: CLOUDINARY_API_SECRET,
+//   api_secret: CLOUDINARY_API_KEY,
+//   secure: true,
+// });
+
+cloudinary.config({ 
+  cloud_name: 'dfaxzahb0', 
+  api_key: '477279579552534', 
+  api_secret: '8H7ZfZznu35WQ3bMH46QEXgWvLg' 
 });
 
 const uploadImage = async (filePath) => {
