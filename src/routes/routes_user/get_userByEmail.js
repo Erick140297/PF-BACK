c250fc351fdcfb8949a70e63ee3b105824c6e6e6
Javy <javixlive@hotmail.com>
2023-01-07 11:19:00 -0600

    Updated getUserByEmail

--- src/routes/routes_user/get_userByEmail.js
+++ src/routes/routes_user/get_userByEmail.js
@@ -5,8 +5,8 @@ const User = require('../../models/user')
 router.get("/userEmail", async (req, res) => {
   try {
     const { email } = req.query
-    // const user = await User.find({email}).populate('services')
-    const user = await User.find({email})
+    const user = await User.find({email}).populate('services')
+    //const user = await User.find({email})
     res.status(200).json(user)
   } catch (error) {
     res.status(400).json({message: error});
