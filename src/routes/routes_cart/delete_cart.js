15b36cd24ff77a9a07265d0553467961f7b3e920
Erick140297 <erickmonterrubio67@outlook.com>
2023-01-08 09:12:54 -0600

    cart

--- src/routes/routes_cart/delete_cart.js
+++ src/routes/routes_cart/delete_cart.js
@@ -4,16 +4,20 @@ const User = require("../../models/user");
 
 const router = express.Router();
 
-router.delete('/cart/', async (req, res) => {
+router.delete('/cart', async (req, res) => {
   try {
-    const {serviceId, cartId, userId} = req.body
+    const {serviceId, cartId} = req.body
     if(serviceId === undefined){
-      await User.findByIdAndUpdate(userId, {cart:[]})
-      await Cart.findByIdAndDelete(cartId)
-      res.status(200).json({ message: "shopping cart was successfully removed" });
+      // await User.findByIdAndUpdate(userId, {cart:[]})
+      // await Cart.findByIdAndDelete(cartId)
+      // res.status(200).json({ message: "shopping cart was successfully removed" });
+      const cart = await Cart.findById(cartId)
+      cart.services = []
+      const cleanCart = await cart.save()
+      res.status(200).json({ message: "cart was successfully cleaned", cleanCart });
     }else{
       const cart = await Cart.findById(cartId)
-      let newServices = cart.services.filter((e) => e == serviceId)
+      let newServices = cart.services.filter((e) => e != serviceId)
       cart.services = newServices
       const savedCart = await cart.save()
       res.status(200).json({ message: "service was successfully removed", savedCart });
